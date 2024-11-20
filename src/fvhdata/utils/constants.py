a666d074126e0828d484cc9137e29688f9e52eb0
Tuomas Tikkanen <tuomas.tikkanen@artific.ai>
2024-11-21 01:52:46 +0200

    Add constants module with hardcoded paths to often used directories

---
+++ src/fvhdata/utils/constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+REPOSITORY_ROOT = Path(__file__).parent.parent.parent.parent
+
+DATA = REPOSITORY_ROOT.joinpath("data")
+RAW = DATA.joinpath("raw")
+INTERIM = DATA.joinpath("interim")
+PROCESSED = DATA.joinpath("processed")
+
+REPORTS = REPOSITORY_ROOT.joinpath("reports")
